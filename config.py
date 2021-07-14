547312655fc6fcf7485d747f0008e1941c371585
Damantha Jasinghe <77870723+Damantha126@users.noreply.github.com>
2021-07-14 21:54:18 +0530

    Update config.py

--- config.py
+++ config.py
@@ -24,7 +24,7 @@ class Config(object):
 	API_ID = int(os.environ.get("API_ID"))
 	API_HASH = os.environ.get("API_HASH")
 	BOT_TOKEN = os.environ.get("BOT_TOKEN")
-	BOT_USERNAME = os.environ.get("BOT_USERNAME")
+	BOTUSERNAME = os.environ.get("BOTUSERNAME")
 	BOT_OWNER = int(os.environ.get("BOT_OWNER"))
 	DATABASE_URL = os.environ.get("DATABASE_URL")
 
